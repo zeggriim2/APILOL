31295485be0dee5132818d8fe9c3eb1eda70c83f
Zeggriim <51760726+zeggriim2@users.noreply.github.com>
2021-06-06 23:35:43 +0200

    index pour les test

--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
-use LolApi\ApiClient;
+
+use Zeggriim\LolApi\ApiClient;
 
 define("PATH_RACINE", dirname(__DIR__));
-define("APIKEY", "RGAPI-3d85f218-1e34-4116-bcc1-e8d16c0b18dc");
+define("APIKEY", "RGAPI-ffc33d4c-b9ba-4436-b0bc-41c94e8d20cb");
+define("LANG", "en_US");
+define("REGION", "euw1");
+define("VERSION", "11.10.1");
 
 require_once PATH_RACINE . DIRECTORY_SEPARATOR . "vendor/autoload.php";
 
+mail('lilian.dorazio@hotmail.fr', 'test', "Message d'un email");
+dd('test');
 
-$apiClient = new ApiClient('fr',APIKEY);
-
-$season = $apiClient->getGeneralApi()->getVersion();
-dump($season);
+$apiClient = new ApiClient(REGION, APIKEY, LANG, VERSION);
+dump($apiClient);
