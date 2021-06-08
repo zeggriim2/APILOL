9d25f2011a932b69d93bbec1dab902f78fd89342
Zeggriim2 <51760726+zeggriim2@users.noreply.github.com>
2021-06-08 22:22:26 +0200

    Récup information pour un champion

--- src/LolApi/API/ChampionApi.php
+++ src/LolApi/API/ChampionApi.php
@@ -5,8 +5,8 @@ namespace Zeggriim\LolApi\API;
 
 class ChampionApi extends BaseApi {
 
-//    const API_CHAMPIONS_URL = "http://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json";
     const API_CHAMPIONS_URL = "http://ddragon.leagueoflegends.com/cdn/{version}/data/{lang}/champion.json";
+    const API_CHAMPION_URL = "http://ddragon.leagueoflegends.com/cdn/{version}/data/{lang}/champion/{champion}.json";
 
     public function getAllChampions()
     {
@@ -17,6 +17,17 @@ class ChampionApi extends BaseApi {
         $url = $this->constructUrl(self::API_CHAMPIONS_URL,$params);
         return $this->callApi($url);
 
+    }
+
+    public function getChampion($nameChampion)
+    {
+        $params = [
+            'version'   => $this->apiClient->getVersion(),
+            'lang'      => $this->apiClient->getLang(),
+            'champion'  => $nameChampion
+        ];
+        $url = $this->constructUrl(self::API_CHAMPION_URL, $params);
+        return $this->callApi($url);
     }
 
     private function constructUrl(string $url, array $params)
